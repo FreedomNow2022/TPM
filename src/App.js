43d873ec52dbc080ca76e41a79a02908705e40e1
FreedomNow2022 <116472643+FreedomNow2022@users.noreply.github.com>
2022-10-22 20:41:16 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>The Poster Movement 2022</h1>
+      <h2>for democracy, for freedom, for lives. </h2>
+    </div>
+  );
+}
